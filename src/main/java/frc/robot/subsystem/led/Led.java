17365c8f5981b3460d385ce87c2a8a7b8bbb471e
abhiiraam kadiam <abhi_kadiam@abhiiraams-MacBook-Air.local>
2020-02-02 15:04:22 -0500

    i commited

--- src/main/java/frc/robot/subsystem/led/Led.java
+++ src/main/java/frc/robot/subsystem/led/Led.java
@@ -9,6 +9,8 @@ package frc.robot.subsystem.led;
 
 import java.util.logging.Logger;
 
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Direction;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.OzoneException;
 import frc.robot.subsystem.PortMan;
@@ -16,16 +18,29 @@ import frc.robot.subsystem.PortMan;
 public class Led extends SubsystemBase {
 
     private static Logger logger = Logger.getLogger(Led.class.getName());
+    private Relay relay0, relay1, relay2, relay3;
 
     public Led() {
 
     }
-      
+     
     public void init(PortMan portMan) throws OzoneException {
       logger.entering(Led.class.getName(), "init()");
+      relay0 = new Relay (portMan.acquirePort(PortMan.relay0_label, "LEDrelay"));
+
+      relay1 = new Relay (portMan.acquirePort(PortMan.relay1_label, "LEDrelay"));
+
+      relay2 = new Relay (portMan.acquirePort(PortMan.relay2_label, "LEDrelay"));
+
+      relay3 = new Relay (portMan.acquirePort(PortMan.relay3_label, "LEDrelay"));
 
       logger.exiting(Led.class.getName(), "init()");
 
+    }
+    public void periodic() {
+      relay0.setDirection(Direction.kReverse);
+      relay0.set(Relay.Value.kOn);
+  
     }
   }
 
